3a946f05780a40ed37dbc311646c1451d4ced9b3
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-09-08 16:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code here
 	let char = "#";
-	Boolean flag = false;
-
+	
 	for(let i = 1; i < shortHex.length; i++){
 		char = char + shortHex.charAt(i) + shortHex.charAt(i);
-		if(shortHex.charAt(i) >= 'A' && sshortHex.charAt(i) <= 'Z'){
-			flag = true;
-		}
 	}
-	if(flag){
-		return char.toUpperCase();
-	}
-	else{
-		return char;
-	}
+	return char;
 };
 
 // Do not change the code below.
