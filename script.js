7377ab876f127c174c9e88cfb1f7bb5d21afd39a
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-09-08 16:40:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const extendHex = (shortHex) => {
   // write your code here
-};
+	let char = "#";
+	
 
+	for(let i = 1; i < shortHex.length; i++){
+		char = char + shortHex.charAt(i) + shortHex.charAt(i);
+	}
+	return char;
+};
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
