e1ab78743edda29b39decde3cd6e26c910254e96
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-09-08 16:51:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const extendHex = (shortHex) => {
   // write your code here
 	let char = "#";
-	
+	Boolean flag = false;
 
 	for(let i = 1; i < shortHex.length; i++){
 		char = char + shortHex.charAt(i) + shortHex.charAt(i);
+		if(shortHex.charAt(i) >= 'A' && sshortHex.charAt(i) <= 'Z'){
+			flag = true;
+		}
 	}
-	return char;
+	if(flag){
+		return char.toUpperCase();
+	}
+	else{
+		return char;
+	}
 };
+
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
